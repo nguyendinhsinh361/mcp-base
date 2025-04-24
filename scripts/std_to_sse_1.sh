1ac597a927dea71fb19bbbce73c1d45377ddc6f8
nguyendinhsinh361 <nguyendinhsinh361@gmail.com>
2025-04-24 23:00:32 +0700

    update newest code

--- scripts/std_to_sse_1.sh
+++ scripts/std_to_sse_1.sh
@@ -3,7 +3,7 @@ cd app/supergateway
 
 yarn build
 yarn start \
-    --stdio "docker run --rm -i -e GITHUB_PERSONAL_ACCESS_TOKEN=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 mcp/github" \
+    --stdio "docker run --rm -i -e GITHUB_PERSONAL_ACCESS_TOKEN= mcp/github" \
     --port 8002 --baseUrl http://192.168.1.34:8002 \
     --ssePath /sse
     
